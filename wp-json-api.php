c8d4d10a3ebcc7104dd150de5d68aa356923aa6f
Praboda Ariyasinghe <prabodamadushan@gmail.com>
2015-09-02 22:26:15 +0530

    Add the plugin file and introductory comment.

---
+++ wp-json-api.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: WP JSON API
+  Plugin URI: http://ceylonsystems.com
+  Description: Boost your WordPress site with a powerful API to integrate it with secondary or a third party.
+  Version: 0.1
+  Author: Ceylon Systems
+  Author URI: http://ceylonsystems.com
+ */
